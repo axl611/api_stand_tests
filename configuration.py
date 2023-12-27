a267993b546feaeb369a6d297d51ee2710818313
axl611 <axl611@gmail.com>
2023-12-27 14:48:56 -0600

    Mi primera chamba

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://e93e5f9f-9156-43f7-a685-66ccf0cf4cb8.serverhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
